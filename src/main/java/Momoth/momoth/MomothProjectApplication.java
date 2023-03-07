b08273f1f876a88065d7b405dd281e896bf84cf6
Shin_Bokyung <bboya0518@naver.com>
2023-03-07 16:34:28 +0900

    2023-03-07 프로젝트 생성

---
+++ src/main/java/Momoth/momoth/MomothProjectApplication.java
@@ -0,0 +1,13 @@
+package Momoth.momoth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomothProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomothProjectApplication.class, args);
+	}
+
+}
