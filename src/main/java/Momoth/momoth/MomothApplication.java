4055ce4747efcc09e3600f5ba03ae0dea4219e02
Shin_Bokyung <bboya0518@naver.com>
2023-03-07 16:50:22 +0900

    20230307

---
+++ src/main/java/Momoth/momoth/MomothApplication.java
@@ -0,0 +1,13 @@
+package Momoth.momoth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomothApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomothApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Momoth/momoth/MomothApplicationTests.java
@@ -0,0 +1,13 @@
+package Momoth.momoth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
